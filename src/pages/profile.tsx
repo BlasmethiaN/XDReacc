0c9431b9dca0337269c1b99e3a3b5027ce927100
BlasmethiaN <dominako223@gmail.com>
2021-03-29 15:16:26 +0200

    logout redirect

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+import { useCurrentUser } from '../api/routes'
 import { CenteredContent, Layout } from '../components/common/Layout.styled'
 import Navbar from '../components/common/Navbar'
 
-const ProfilePage = () => (
-  <Layout>
-    <Navbar />
-    <CenteredContent style={{ display: 'flex' }}>something</CenteredContent>
-  </Layout>
-)
+const ProfilePage = () => {
+  const router = useRouter()
+  const { data: currentUser } = useCurrentUser()
+
+  useEffect(() => {
+    if (!currentUser) {
+      router.replace('/')
+    }
+  }, [currentUser])
+
+  return (
+    <Layout>
+      <Navbar />
+      <CenteredContent style={{ display: 'flex' }}>something</CenteredContent>
+    </Layout>
+  )
+}
 
 export default ProfilePage
